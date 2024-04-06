f19cdc4d14853a921254b95b7f14bf34bc28bf72
youngchangyoo <ddori5338@hanmail.net>
2024-04-07 02:39:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/Movies/MoviePage';
+import MovieDetailPage from './pages/MovieDetail/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import AppLayout from './layout/AppLayout';
 
+// 홈페이지       /
+// 서치 페이지    /movies
+// 디테일 페이지  /movies/:id
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout/>}>
+        <Route index element={<Homepage/>}/>
+        <Route path="movies">
+          <Route index element={<MoviePage/>}/>
+          <Route path=":id" element={<MovieDetailPage/>}/>
+        </Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes>
   );
 }
 
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
+import './AppLayout.style.css';
+import { useNavigate } from "react-router-dom";
+
+const AppLayout = () => {
+    const navigate = useNavigate();
+    const goToHome = () => {
+        navigate("/");
+    }
+    return (
+        <div>
+            <Navbar variant="dark" expand="lg" className="bg-body-tertiary nb">
+                <Container fluid>
+                    <img width={120} src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" className="logo" alt="" onClick={goToHome}/>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="/" className="text-white">Home</Nav.Link>
+                        <Nav.Link href="movies" className="text-white">Movies</Nav.Link>
+
+                    </Nav>
+                    <Form className="d-flex">
+                        <Form.Control variant="outline-danger"
+                        type="search"
+                        placeholder="Search"
+                        className="me-2"
+                        aria-label="Search"
+                        />
+                        <Button variant="outline-danger" className="search-btn">Search</Button>
+                    </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Outlet/>
+        </div>
+    )
+}
+
+export default AppLayout
