469ed0a42964c388f24cbed2307fdb4733ce0028
youngchangyoo <ddori5338@hanmail.net>
2024-04-10 00:38:36 +0900

    mapped genre id with name

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import Badge from 'react-bootstrap/Badge';
 import './MovieCard.style.css'
+import { useMovieGenreQuery } from '../../hooks/useMovieGenre';
 
 const MovieCard = ({movie}) => {
+
+    const {data:genreData} = useMovieGenreQuery()
+    
+    const showGenre = (genreIdList) => {
+        if (!genreData) return []
+        const genreNameList = genreIdList.map((id) => {
+            const genreObj = genreData.find((genre) => genre.id === id)
+            return genreObj.name
+        })
+        return genreNameList
+    }
     return (
         <div
         style={{backgroundImage: `url(https://media.themoviedb.org/t/p/w300_and_h450_bestv2${movie.poster_path})`}}
         className='movie-card'>
             <div className='overlay'>
                 <h2>{movie.title}</h2>
-                {movie.genre_ids.map((id) => <Badge bg="danger">{id}</Badge>)}
+                {showGenre(movie.genre_ids).map((id) => <Badge bg="danger">{id}</Badge>)}
                 <div className='info'>
                     <div className='vote-avg'>{Math.round(movie.vote_average*10)}% Match</div>
                     <div>{movie.release_date}</div>
