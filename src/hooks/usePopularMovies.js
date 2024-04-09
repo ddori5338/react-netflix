4ef28743eb59ec4590a3ff3489fe778da404e393
youngchangyoo <ddori5338@hanmail.net>
2024-04-10 00:53:17 +0900

    added TopRated, Upcoming Line

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -8,6 +8,7 @@ export const usePopularMoviesQuery = () => {
     return useQuery({
         queryKey: ['movie-popular'],
         queryFn: fetchPopularMovies,
-        select: (result) => result.data
+        select: (result) => result.data,
+        staleTime: 300000
     })
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Banner from './components/Banner/Banner'
 import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide'
+import TopRatedMovieSlide from './components/TopRatedMovieSlide/TopRatedMovieSlide'
+import UpcomingMovieSlide from './components/UpcomingMovieSlide/UpcomingMovieSlide'
 
 // 1. 배너 => popular movie 첫 번째 아이템을 보여주자
 // 2. popular movie
@@ -11,6 +13,8 @@ const Homepage = () => {
         <div>
             <Banner/>
             <PopularMovieSlide/>
+            <TopRatedMovieSlide/>
+            <UpcomingMovieSlide/>
         </div>
     )
 }
